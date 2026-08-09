2e756d9b710813bb874721ddc6e64f86bce33271
teet44182-sketch <teet44182@gmail.com>
2026-08-09 23:09:53 +0700

    hw3

---
+++ aima/python-tutorial/tutorial_code/exercises/addition.py
@@ -0,0 +1,17 @@
+# addition.py
+# -----------
+
+
+"""
+Run python autograder.py
+"""
+
+
+def add(a, b):
+    "Return the sum of a and b"
+    "*** YOUR CODE HERE ***"
+    c =a+b
+    return c
+
+
+
